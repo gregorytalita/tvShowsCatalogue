1900c18351f4a425d7e5aacb0ca79c7d1196fd62
Talita Gregory <talita.gregory@tenzinger.com>
2021-02-19 15:49:36 +0100

    loader removed

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { Switch, Redirect } from 'react-router-dom'
 
 import TVShowsContext from '../../contexts/TVShowsContext'
-import { TemplateRoute, LoaderOverlay } from '../../components'
+import { TemplateRoute } from '../../components'
 import * as routes from '../../core/constants/routes'
 
 import { searchShows } from '../../core/api/search'
@@ -14,51 +14,43 @@ import SearchResult from '../SearchResult'
 
 const App = ({ history }) => {
   const [tvShows, handleTVShows] = useState({})
-  const [loading, handleLoading] = useState(true)
 
   useEffect(() => {
     getShows()
       .then(formatIntoGenres)
       .then(handleTVShows)
-      .then(() => handleLoading(false))
   }, [])
 
   const onClickSearch = search => {
-    handleLoading(true)
 
     searchShows(search)
       .then(searchResult => {
+
         history.push({
           pathname: routes.NOT_PRIVATE.SEARCH,
-          state: {
-            search,
-            shows: searchResult
-          }
+          state: { search, shows: searchResult }
         })
 
-        handleLoading(false)
+
       })
   }
 
   return (
-    <>
-      {/* <LoaderOverlay enabled={loading}/> */}
-      <TVShowsContext.Provider value={{ showsByGenre: tvShows }}>
-        <Switch>
-          <TemplateRoute
-            path={routes.NOT_PRIVATE.DASHBOARD}
-            component={Dashboard}
-            onClickSearch={onClickSearch}
-          />
-          <TemplateRoute
-            path={routes.NOT_PRIVATE.SEARCH}
-            component={SearchResult}
-            onClickSearch={onClickSearch}
-          />
-          <Redirect from='*' to={routes.NOT_PRIVATE.DASHBOARD} />
-        </Switch>
-      </TVShowsContext.Provider>
-    </>
+    <TVShowsContext.Provider value={{ showsByGenre: tvShows }}>
+      <Switch>
+        <TemplateRoute
+          path={routes.NOT_PRIVATE.DASHBOARD}
+          component={Dashboard}
+          onClickSearch={onClickSearch}
+        />
+        <TemplateRoute
+          path={routes.NOT_PRIVATE.SEARCH}
+          component={SearchResult}
+          onClickSearch={onClickSearch}
+        />
+        <Redirect from='*' to={routes.NOT_PRIVATE.DASHBOARD} />
+      </Switch>
+    </TVShowsContext.Provider>
   )
 }
 
--- src/containers/SearchResult/index.jsx
+++ src/containers/SearchResult/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 
 import { MovieCard, DetailsDrawer, DetailsTemplate } from '../../components'
 import { Box, Typography, Grow } from '@material-ui/core'
