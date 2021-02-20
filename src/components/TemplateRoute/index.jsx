79c80448825553342e75228fb29e963889e61d87
Talita Gregory <talita.gregory@tenzinger.com>
2021-02-20 19:48:05 +0100

    unused component removed

--- src/components/TemplateRoute/index.jsx
+++ src/components/TemplateRoute/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
-import Template from '../../Template'
+import Template from '../Template'
 
 const TemplateRoute = ({ component: Component, onKeyUpSearch, redirectRoute, ...rest }) => {
   return (
